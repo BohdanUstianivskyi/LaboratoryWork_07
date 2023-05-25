ac664ae667c84b413094957000e4414ff11c4c01
BohdanUstianivskyi <bohdan.ustianivskyi.tr.2021@lpnu.ua>
2023-05-25 22:24:40 +0300

    Lab07

--- src/Exercise_01.java
+++ src/Exercise_01.java
@@ -91,18 +91,52 @@ public class Exercise_01{
         List<Car> cars = new ArrayList<>();
 
         // Створення 12 машинок
-        Driver driver1 = new Driver("Водій 1", 30, 5);
-        Car car1 = new Car("Марка 1", 100, driver1, 10000, 2010);
+        Driver driver1 = new Driver("Ivan", 30, 5);
+        Car car1 = new Car("Honda", 100, driver1, 10000, 2010);
         cars.add(car1);
 
-        Driver driver2 = new Driver("Водій 2", 35, 7);
-        Car car2 = new Car("Марка 2", 120, driver2, 15000, 2012);
+        Driver driver2 = new Driver("Yaroslav", 35, 1);
+        Car car2 = new Car("Lexus", 120, driver2, 15000, 2012);
         cars.add(car2);
 
-        // Додаткові машинки...
+        Driver driver3 = new Driver("Vasyl", 31, 6);
+        Car car3 = new Car("Chevrolet", 87, driver3, 6000, 2010);
+        cars.add(car3);
 
-        Driver driver12 = new Driver("Водій 12", 27, 3);
-        Car car12 = new Car("Марка 12", 90, driver12, 8000, 2008);
+        Driver driver4 = new Driver("Nazar", 29, 7);
+        Car car4 = new Car("Mitsubishi", 140, driver4, 13000, 2009);
+        cars.add(car4);
+
+        Driver driver5 = new Driver("Andriy", 25, 5);
+        Car car5 = new Car("Mazda", 125, driver5, 7800, 2010);
+        cars.add(car5);
+
+        Driver driver6 = new Driver("Ihor", 20, 2);
+        Car car6 = new Car("Tavria", 69, driver6, 1500, 2003);
+        cars.add(car6);
+
+        Driver driver7 = new Driver("Orest", 23, 4);
+        Car car7 = new Car("KIA", 130, driver7, 11000, 2015);
+        cars.add(car7);
+
+        Driver driver8 = new Driver("Natalia", 21, 3);
+        Car car8 = new Car("Nissan", 115, driver8, 9000, 2013);
+        cars.add(car8);
+
+        Driver driver9 = new Driver("Vitaliy", 19, 6);
+        Car car9 = new Car("Skoda", 137, driver9, 8000, 2012);
+        cars.add(car9);
+
+        Driver driver10 = new Driver("Andriy", 19, 4);
+        Car car10 = new Car("Opel", 115, driver10, 6000, 1997);
+        cars.add(car10);
+
+        Driver driver11 = new Driver("Stepan", 39, 5);
+        Car car11 = new Car("BMW", 137, driver11, 9500, 2003);
+        cars.add(car11);
+
+        Driver driver12 = new Driver("Bohdan", 19, 7);
+        Car car12 = new Car("Daewoo", 110, driver12, 8000, 2008);
         cars.add(car12);
 
         // Ремонт половини автопарку
--- src/Exercise_02.java
+++ src/Exercise_02.java
@@ -1,2 +1,46 @@
-package PACKAGE_NAME;public class Exercise_02 {
+import java.util.HashMap;
+import java.util.Map;
+
+public class Exercise_02 {
+    public static void main(String[] args) {
+        // Створення об'єкту HashMap для зберігання прізвищ та номерів телефонів
+        Map<String, String> phoneBook = new HashMap<>();
+
+        // Додавання елементів в карту
+        phoneBook.put("Карпенко", "+380951234567");
+        phoneBook.put("Ткач", "+380661234567");
+        phoneBook.put("Білоус", "+380631234567");
+        phoneBook.put("Коваленко", "+380991234567");
+        phoneBook.put("Вовк", "+380971234567");
+        phoneBook.put("Ковальчук", "+380681234567");
+        phoneBook.put("Мельник", "+380631234567");
+        phoneBook.put("Бондаренко", "+380501234567");
+        phoneBook.put("Шевченко", "+380961234567");
+        phoneBook.put("Тищенко", "+380931234567");
+
+        // Виведення інформації з книги за допомогою циклу
+        for (Map.Entry<String, String> entry : phoneBook.entrySet()) {
+            String lastName = entry.getKey();
+            String phoneNumber = entry.getValue();
+            System.out.println("Прізвище: " + lastName + ", Номер телефону: " + phoneNumber);
+        }
+
+        // Знайдення номеру за прізвищем
+        String lastNameToFind = "Шевченко";
+        if (phoneBook.containsKey(lastNameToFind)) {
+            String phoneNumber = phoneBook.get(lastNameToFind);
+            System.out.println("Номер телефону для прізвища " + lastNameToFind + ": " + phoneNumber);
+        } else {
+            System.out.println("У книзі відсутній абонент з прізвищем " + lastNameToFind);
+        }
+
+        // Видалення запису з книги
+        String lastNameToRemove = "Мельник";
+        phoneBook.remove(lastNameToRemove);
+        System.out.println("Запис з прізвищем " + lastNameToRemove + " видалено.");
+
+        // Виведення кількості записів у книзі
+        int numberOfEntries = phoneBook.size();
+        System.out.println("Кількість записів у книзі: " + numberOfEntries);
+    }
 }
